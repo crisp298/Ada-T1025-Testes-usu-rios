f1533f7849089103454a0399f61a9bad2addaba9
vitoralbq <vitor.albuquerque.mun@gmail.com>
2023-11-24 16:08:23 -0300

    Resolvendo bugs

--- src/useCases/creatStudentUseCase.ts
+++ src/useCases/creatStudentUseCase.ts
@@ -3,28 +3,23 @@ import  Student from "../models/Student";
 import createHttpError from "http-errors";
 import { IStudentsRepository } from "../repositories/StudentsRepository";
 
-@injectable
+@injectable()
 export class CreateStudent
  {
   constructor(
     @inject("StudentsRepository") private StudentsRepository: IStudentsRepository){};
 
     async execute(student: Student){
-        if(
-            !student.name ||
-            !student.shift ||
-            !student.year ||
-            !student.room ||
+        if(!student.name||!student.shift||!student.year||!student.room)
 
-        ){
-        
+        {
             throw createHttpError.BadRequest("Propriedades obirgatórias ausentes no corpo da requisição.")
         }
 
         const studentsDetails: any[] =[];
 
         try{
-            const newStudent = await.this.studentsRepository.createStudent(student);
+            const newStudent = await this.StudentsRepository.createStudent(student);
             
             return newStudent;
         } catch(error) {
